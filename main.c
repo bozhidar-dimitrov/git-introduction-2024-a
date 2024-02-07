1639dcf685c3bcbccc6a2adaeedcc9358068116f
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2024-02-07 14:07:53 +0200

    This is commit from the terminal

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define MAX_SIZE 5
+
 int main(void) {
 
   printf("?Hello from C-new?\n");
   printf("This is done in local machine\n");
+
+  int arr[MAX_SIZE] = {1, 2, 3, 4, 5};
 
   return 0;
 }
