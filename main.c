b14078500da41061c70b1ea5a3ad2747e266cc0c
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2024-02-07 14:19:31 +0200

    New core feature

--- main.c
+++ main.c
@@ -4,10 +4,15 @@
 
 int main(void) {
 
+
+
   printf("?Hello from C-new?\n");
   printf("This is done in local machine\n");
 
   int arr[MAX_SIZE] = {1, 2, 3, 4, 5, 6, 7};
+  for (int i = 0; i < MAX_SIZE; i++) {
+    printf("%d ", arr[i]);
+  }
 
   return 0;
 }
