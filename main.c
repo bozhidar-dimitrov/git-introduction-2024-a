f2d0dcec09a619459e5f7f68f0dd92613eaf23b5
Bozhidar <bozhidar.dimitrov@gmail.com>
2024-02-07 14:09:40 +0200

    Update max size

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define MAX_SIZE 5
+#define MAX_SIZE 7
 
 int main(void) {
 
   printf("?Hello from C-new?\n");
   printf("This is done in local machine\n");
 
-  int arr[MAX_SIZE] = {1, 2, 3, 4, 5};
+  int arr[MAX_SIZE] = {1, 2, 3, 4, 5, 6, 7};
 
   return 0;
 }
